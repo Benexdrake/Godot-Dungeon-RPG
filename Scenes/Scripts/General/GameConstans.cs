3e92f61f9436a262efe4305272d69d06e012799a
BeneX Drake <benex@hotmail.de>
2024-04-14 10:48:58 +0200

    update

---
+++ Scenes/Scripts/General/GameConstans.cs
@@ -0,0 +1,12 @@
+public class GameConstants
+{
+    // Animation
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+
+    // Input
+    public const string INPUTS_MOVE_LEFT = "MoveLeft";
+    public const string INPUTS_MOVE_RIGHT = "MoveRight";
+    public const string INPUTS_MOVE_FORWARD = "MoveForward";
+    public const string INPUTS_MOVE_BACKWARD = "MoveBackward";
+}
